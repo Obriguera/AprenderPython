b3e6a494723da1de6c7bcd7b360ab40edd33d15f
Obriguera <Obriguera03@gmail.com>
2024-04-06 02:36:57 -0300

    Primeros 15 ejercicios de https://www.w3resource.com/python-exercises/python-basic-exercises.php

---
+++ EjerciciosPagina/015.py
@@ -0,0 +1,9 @@
+#Write a Python program to get the volume of a sphere with radius six.
+# V = (4/3)*Pi*r^3
+# r = 6
+
+radio = 6
+radio_cubo = 6*6*6
+
+volumen = (4/3) * 3.15 * radio_cubo
+print(volumen)
